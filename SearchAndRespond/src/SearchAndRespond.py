55643248767ef163225baf9eed9f3b3c0fe68ff2
ben0815 <ben0815@tamu.edu>
2017-05-30 09:32:42 -0500

    SearchAndRespond.ini -> praw.ini

--- SearchAndRespond/src/SearchAndRespond.py
+++ SearchAndRespond/src/SearchAndRespond.py
@@ -2,7 +2,6 @@
 
 import praw
 
-reddit = praw.Reddit(client_id='my client id'), client_secret='my client secret', user_agent='my user agent')
-
-print(reddit.read_only)
+reddit = praw.Reddit('Authentication')
+print(reddit.user.me())
 
