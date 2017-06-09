a3eeda3b5b6b741592ff8bf167aadb70efbd239a
ben0815 <ben0815@tamu.edu>
2017-06-09 10:40:21 -0500

    Completed random image poster bot. Randomly posts images from provided imgur album to a given subreddit.

--- RandomImagePoster/README.md
+++ RandomImagePoster/README.md
@@ -1,7 +1,8 @@
 # RandomImagePoster
 
-Reddit bot to pick random images from a album and post them to a subreddit.
+Reddit bot to pick random images from an imgur album and posts them to a subreddit.
 Written by /u/benagin. Requested by /u/PM_ME__STUFF.
 
-Use input.json to provide the path to the images, frequency of the posts in
-hours, and the subreddit to post to.
+Use input.json to provide the imgur album id, frequency of the posts in
+hours, the subreddit to post to, and the frequency to repeat image posts in days.
+The path to the album must lead to the directory containing the image files.
--- RandomImagePoster/src/RandomImagePoster.py
+++ RandomImagePoster/src/RandomImagePoster.py
@@ -1,34 +1,107 @@
 #!/usr/local/bin/python3
 
 import praw
+from imgurpython import ImgurClient
+from imgurpython.helpers.error import ImgurClientError
 import time
+import threading
+import random
 import json
 from os import listdir
 from os.path import isfile, join
 
 
-# Initialize reddit object with .ini file. Continuously pick a random image from
-# the album directory and post it to the input subreddit. Wait for a given
-# number of hours before posting again.
+# Initialize reddit object with .ini file.
 reddit = praw.Reddit('Authentication')
 
-def main():
-    print("Successfully logged into Reddit with username ", reddit.user.me(),
-            ".", sep = '')
+# Imgur authentication information.
+client_id = 'insert_client_id'
+client_secret = 'insert_client_secret'
 
-    # Load JSON file for album directory, frequency of posts, and subreddit.
+# Connect to imgur.
+try:
+    client = ImgurClient(client_id, client_secret)
+
+except ImgurClientError as e:
+    print(e.error_message)
+    print(e.status_code)
+
+# Cache image files so that we don't repost the same image.
+# This cache is cleared every x days. x is provided in the input.json file.
+cache = []
+
+# Continuously pick a random image from the imgur album and post it to the input
+# subreddit. Wait for a given number of hours before posting again.
+# Cache images so the are not repeated. Clear cache according to input
+# frequency.
+def main():
+    # Check if authentication information is correct.
+    try:
+        print("Successfully logged into Reddit with username ", reddit.user.me(),
+                ".", sep = '')
+
+    except Exception as e:
+        print(e)
+        return
+
+    # Load JSON file for album id, frequency of posts, subreddit, and
+    # frequency to repeat image posts.
     with open('input.json') as input_file:
         input = json.load(input_file)
 
+    # Execute thread to clear cache.
+    thread = threading.Thread(target = clear_cache, args =
+            (input['repeat_frequency'],))
+    thread.start()
+
     while(True):
-        files = [f for f in listdir(input['album_path'])
-                if isfile(join(input['album_path'], f))]
+        # Get all images in the imgur album.
+        try:
+            images = client.get_album_images(input['album_id'])
+            for image in images:
+                print(image.link)
 
-        for f in files:
-            print(f)
+        except Exception as e:
+            print(e)
+            break;
+
+        # Pick a random image until we get one that isn't cached.
+        while(True):
+            index = random.randrange(0, len(images))
+
+            # Cache miss. Post this image.
+            link = images[index].link
+            if link not in cache:
+                print("Caching image ", link, ".", sep = '')
+                cache.append(link)
+                post(link, input['subreddit'])
+                break;
+
+        # Wait for a specific number of hours. This is provided in the
+        # input.json file.
+        hours = input['frequency']
+        print("sleeping for", hours, "hours...")
+
+        # Convert hours to seconds.
+        time.sleep(hours * 60 * 60)
 
 
+def post(link, subreddit):
+    print("Posting the image ", link, " to the subreddit ", subreddit,
+            "...", sep = '')
 
+    sub = reddit.subreddit(subreddit)
+    sub.submit("Image of the day! (Test)", url = link)
+
+
+def clear_cache(days):
+    print("Sleeping for", days, "days before clearing the cache...")
+
+    # Convert days to seconds.
+    time.sleep(days * 24 * 60 * 60)
+
+    print("Cache cleared.")
+    del cache[:]
 
 
 if __name__ == '__main__':
